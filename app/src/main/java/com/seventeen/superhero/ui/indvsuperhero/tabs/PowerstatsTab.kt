559f28b5abc33b9304f2109edb626462731f92a0
sushruth17 <sushruth.sushe@gmail.com>
2021-03-31 04:13:22 +0530

    circular progress view

--- app/src/main/java/com/seventeen/superhero/ui/indvsuperhero/tabs/PowerstatsTab.kt
+++ app/src/main/java/com/seventeen/superhero/ui/indvsuperhero/tabs/PowerstatsTab.kt
@@ -1,16 +1,19 @@
 package com.seventeen.superhero.ui.indvsuperhero.tabs
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import at.grabner.circleprogress.CircleProgressView
 import com.seventeen.superhero.R
-import com.seventeen.superhero.databinding.FragmentBiographyTabBinding
 import com.seventeen.superhero.databinding.FragmentPowerstatsTabBinding
 import com.seventeen.superhero.ui.indvsuperhero.IndvSuperheroFragmentArgs
 import com.seventeen.superhero.utils.UtilsString
 
 class PowerstatsTab : Fragment(R.layout.fragment_powerstats_tab)  {
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -23,8 +26,17 @@ class PowerstatsTab : Fragment(R.layout.fragment_powerstats_tab)  {
             strength.text = "Strength: ${powerstats?.strength}"
             speed.text = "Speed: ${powerstats?.speed}"
             durability.text = "Durability: ${powerstats?.durability}"
-            power.text = "power: ${powerstats?.power}"
+            power.text = "Power: ${powerstats?.power}"
             combat.text = "Combat: ${powerstats?.combat}"
+
+            circularView.maxValue = 100F
+            circularView.setValueAnimated(
+                    0F, powerstats?.intelligence!!.toFloat(),
+                    2000L)
+            circularView.setText("intell")
+            circularView.isAutoTextSize = true
+            circularView.setTextColorAuto(true)
+            circularView.transitionName = "adhsjhd"
 
         }
     }
