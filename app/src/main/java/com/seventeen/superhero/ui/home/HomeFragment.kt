8ff074c159692ba870ff80527c68b39d4ab79eb7
sushruth17 <sushruth.sushe@gmail.com>
2021-03-30 23:15:21 +0530

    theme change + home view model correction

--- app/src/main/java/com/seventeen/superhero/ui/home/HomeFragment.kt
+++ app/src/main/java/com/seventeen/superhero/ui/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.seventeen.superhero.ui.home
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -13,6 +14,7 @@ import com.seventeen.superhero.R
 import com.seventeen.superhero.data.ResultsItem
 import com.seventeen.superhero.data.SuperheroResponse
 import com.seventeen.superhero.databinding.FragmentHomeBinding
+import com.seventeen.superhero.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -51,9 +53,9 @@ class HomeFragment : Fragment(R.layout.fragment_home), HomeAdapter.OnItemClickLi
 
                 progressBar.visibility = View.GONE
 
-//                progressBar.isVisible = result is Resource.Loading<*>
-//                textViewError.isVisible = result is Resource.Error<*>
-//                textViewError.text
+//                progressBar.isVisible = result is Resource.Loading<*> && result.isNullOrEmpty()
+//                textViewError.isVisible = result is Resource.Error<*> && result.isNullOrEmpty()
+//                textViewError.text = result.error.localizedMessage
             }
 
         }
--- app/src/main/java/com/seventeen/superhero/ui/indvsuperhero/IndvSuperheroFragment.kt
+++ app/src/main/java/com/seventeen/superhero/ui/indvsuperhero/IndvSuperheroFragment.kt
@@ -2,11 +2,13 @@ package com.seventeen.superhero.ui.indvsuperhero
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.seventeen.superhero.R
 import com.seventeen.superhero.databinding.FragmentIndvSuperheroBinding
+
 
 class IndvSuperheroFragment: Fragment(R.layout.fragment_indv_superhero) {
 
@@ -18,10 +20,14 @@ class IndvSuperheroFragment: Fragment(R.layout.fragment_indv_superhero) {
 
         val binding = FragmentIndvSuperheroBinding.bind(view)
 
+        (activity as AppCompatActivity?)!!.supportActionBar!!.title = args.result.name
+
+
         binding.apply {
             Glide.with(this@IndvSuperheroFragment)
                 .load(args.result.image?.url)
                 .into(imv)
+
         }
 
 
