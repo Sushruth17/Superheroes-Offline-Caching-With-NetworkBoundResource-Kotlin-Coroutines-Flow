cb258934158f43906fc90ab6f94613c227ec4252
sushruth17 <sushruth.sushe@gmail.com>
2021-03-30 13:10:47 +0530

    initial commit

---
+++ app/src/main/java/com/seventeen/superhero/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.seventeen.superhero.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
